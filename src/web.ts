b5142af0b310ce6212103f21bf8521449857a957
Tomás Cichero <tomascichero@gmail.com>
2022-04-13 01:34:03 -0300

    updated config

---
+++ src/web.ts
@@ -0,0 +1,2 @@
+import NavKeysController from ".";
+window.NavKeysController = NavKeysController
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist_web'),
-    },
-    devServer: {
-        static: {
-          directory: path.join(__dirname, 'test'),
+module.exports = (env) => {
+    return {
+        entry: env.production ? './src/web.ts' : './src/index.ts',
+        mode: env.production ? 'production' : 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
         },
-        compress: true,
-        port: 8080,
-      },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist_web'),
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'test'),
+            },
+            compress: true,
+            port: 8080,
+        },
+    }
 };
