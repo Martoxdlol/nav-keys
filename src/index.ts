8d29f6d7ce62041a9c65eb5b8354e2c507f0d4f1
Tomás Cichero <martoxdlol@gmail.com>
2022-04-18 12:14:10 -0300

    all new source code

--- src/index.ts
+++ src/index.ts
@@ -1,299 +1,207 @@
-export type Options = {
-    initialUrl?: Location
+/*
+How it works:
+
+Native browser history will have three positions: [state: {pos:0}], [state: {pos:1}], [state: {pos:2}] (the last one is optional)
+Normally it will stay always on {pos:1}, when user click back button it will trigger a 'popstate' event and the new current state will be {pos:0}
+because of that I can know the user clicked back.
+If user click forward button it will trigger a 'popstate' event and the new current state will be {pos:2}
+
+Brwoser native history states:
+{pos: 0}, {pos: 1}, {pos: 2}
+    Normal state^
+
+{pos: 0}, {pos: 1}, {pos: 2}
+      ^ On back
+
+{pos: 0}, {pos: 1}, {pos: 2}
+                On forward^ 
+
+{pos: 0}, {pos: 1}
+      Forwawrd disabled ^
+
+After each event, it will return to normal state
+*/
+
+export type NavKeysOptions = {
+    initialUrl?: string
+    /* It calls last added listener first if enabled */
+    callListenersAsStack?: boolean
     allowHashchange: boolean
     listenHashchange: boolean
 }
 
-const DEFAULT_OPTIONS: Options = {
-    allowHashchange: true,
+
+export const DEFAULT_OPTIONS: NavKeysOptions = {
+    allowHashchange: false,
     listenHashchange: true,
 }
 
-// Why not
-export { DEFAULT_OPTIONS }
-
-enum Action {
-    forward = "forward",
-    back = "back",
-    hashchange = "hashchange",
+export enum Action {
+    forward = 'forward',
+    back = 'back',
+    hashchange = 'hashchange',
 }
 
-export type NavigationEvent = {
-    action: Action
-    location: URL
-    isHashChange: boolean
-    stopPropagation?: Function
+export type NavKeyEvent = {
+    action: Action,
+    url: URL,
 }
 
-type CreatePromiseResult = {
-    promise: Promise<void>
-    resolver: Function
-}
+export default class NavKeysController {
+    options: NavKeysOptions
+    nativeHistory: History
+    private _url: string
 
-function createPromise(): CreatePromiseResult {
-    let resolver
-    return {
-        promise: new Promise((resolve, reject) => {
-            resolver = resolve
-        }),
-        resolver
-    }
-}
+    // required state of forward button
+    private _forwardButtonEnabledNext: boolean
+    // real state of forward button
+    private _forwardButtonEnabled: boolean
+    // Queue of events
+    private eventQueue: Array<NavKeyEvent> = []
 
-class NavKeysController {
-    // General purpose options
-    readonly options: Options
+    private listeners: Set<Function> = new Set()
 
-    // Browser real history
-    private originalHistory: History
-
-    // toggle this on when popstate event is espected to happen
-    //0 == false
-    private ignoreEvent: number
-
-    // keeps track of forward button enable state
-    private forwardButtonEnabled: boolean
-
-    // awaiting this ensures that native history events finished it cycle. Also, handles popstate event
-    private waitEventTrigger: Function
-
-    // Actual location/url
-    private _url: URL
-
-    // Event listeners set
-    private listeners: Set<Function>
-
-    // Temporal variables used on event trigger cycle
-    private _nextUrl?: URL
-    private _disableForward: boolean
-    private _enableForward: boolean
-
-    constructor(originalHistory: History = window.history, options?) {
-        this.options = { ...DEFAULT_OPTIONS, ...options }
-        this.originalHistory = originalHistory
-        this.ignoreEvent = 0
-        this.forwardButtonEnabled = false
-        this.listeners = new Set()
-        this._url = new URL(window.location.toString())
-
-        if (this.options.initialUrl) {
-            this._url = options.initialLocation
+    constructor(history?: History, options?: NavKeysOptions) {
+        options = { ...DEFAULT_OPTIONS, ...options }
+        this.options = options
+        this.nativeHistory = history || window.history
+        this._url = window.location.href
+        if (options.initialUrl) {
+            // Complete the route if given initialUrl was not a full url
+            this._url = (new URL(options.initialUrl, this._url)).href
         }
 
-        //Inicializar sistema
-        this.initHistory(this._url.href)
+        // Initialize history states
+        initHistory(this.nativeHistory, this._url)
 
-        //handles popstate event
-        this.waitEventTrigger = this.handleEvent()
-
-        //Used to replace url when triggered event
-        this._nextUrl = null
+        // We use 'hashchange' to ensure it works on IE
+        window.addEventListener("hashchange", e => {
+            const u = window.location.href
+            if (options.allowHashchange) {
+                this._url = u
+            }
+            this.nativeHistory.replaceState({ pos: 2 }, '', this._url)
+            this._lastPos = 2
+            this.nativeHistory.back()
+            this._preventBackEvent++
+            this.nativeHistory.back()
+            if (options.listenHashchange) {
+                this.eventQueue.push({ action: Action.hashchange, url: new URL(u) })
+            }
+        }, false)
+        window.addEventListener('popstate', this.handleEvent.bind(this))
     }
 
-    private initHistory(initialUrl: string) {
-        // HISTORY STATES: [ state = {pos: 0 } ]       [ state = {pos: 1 } ]             [ state = {pos: 2 } ]   => the last block can be romeved to disable forward button
-        //       USED TO DETECT BACK                  KEEP TEH USER ALLWAYS HERE        USED TO DETECT FORWARD
-        // If user goes back: the event handler will detect pos = 0 and take action as back event
-        // If user goes forward: the event handler will detect pos = 2 and take action as forward event
-        // If user use hash navigation: the event handler will detect pos = undefined and take action as navigate event
-        this.originalHistory.replaceState({ pos: 0 }, '', initialUrl)
-        this.originalHistory.pushState({ pos: 1 }, '', initialUrl)
+    private _lastPos: number = 1
+    private _preventBackEvent: number = 0
 
-        this.originalHistory.scrollRestoration = 'manual'
-    }
+    private handleEvent(event: PopStateEvent) {
+        const state = event.state || {}
+        const pos = state.pos ?? null
 
-    async _enableForwardButton() {
-        //Prevent handler from doing strange stuff
-        this.ignoreEvent++
-        //Ensure there are a 3rd block (pos: 2)
-        this.originalHistory.pushState({ pos: 2 }, '')
-        //Returns to pos:1
-        this.originalHistory.back()
-        //Wait event call
-        await this.waitEventTrigger()
-        //Keep track of state
-        this.forwardButtonEnabled = true
-        //
-        this.ignoreEvent--
-    }
+        if (pos === 0) {
+            // Back click
 
-    async enableForwardButton() {
-        if (this.ignoreEvent) {
-            this._enableForward = true
-        } else {
-            await this._enableForwardButton()
-        }
-    }
+            // Always required actions, (return to pos:1 and if neccessary disable forward)
+            this.nativeHistory.replaceState(state, '', this._url)
+            this._lastPos = 0
 
-    private async _disableForwardButton() {
-        //new pushed url
-        const href = this.url
-        //Prevent handler from doing strange stuff
-        this.ignoreEvent++
-        //return to pos 1 from pos undefined ==> 2
-        this.originalHistory.back()
-        //wait back event finish
-        await this.waitEventTrigger()
-        //set correct url
-        this.originalHistory.pushState({ pos: 1 }, '', href.toString())
-        //Keeps track of forwardButtonEnabled
-        this.forwardButtonEnabled = false
-        //
-        this.ignoreEvent--
-    }
-
-    async disableForwardButton() {
-        if (this.ignoreEvent) {
-            this._disableForward = true
-        } else {
-            await this._disableForwardButton()
-        }
-    }
-
-    get url() {
-        return this._url
-    }
-
-    set url(url: URL | string) {
-        this._url = new URL(url.toString(), this._url)
-        if (this.ignoreEvent) {
-            this._nextUrl = this._url
-        } else {
-            //Replace url
-            this.originalHistory.replaceState(this.originalHistory.state, '', url.toString())
-        }
-    }
-
-    private launchEvent(action: Action, customEventData: NavigationEvent): void {
-        customEventData.action = action
-        const list = Array.from(this.listeners.values())
-        let cb = list.pop()
-        let next = true
-        customEventData.stopPropagation = () => {
-            next = false
-        }
-        while (cb && next) {
-            cb(customEventData)
-            cb = list.pop()
-        }
-    }
-
-    public listen(callback: Function) {
-        this.listeners.add(callback)
-        return () => {
-            this.listeners.delete(callback)
-        }
-    }
-
-    public exit() {
-        this.ignoreEvent++
-        this.originalHistory.back()
-        this.originalHistory.back()
-    }
-
-    get isForwardButtonEnabled() {
-        return this.forwardButtonEnabled
-    }
-
-    private handleEvent() {
-        let { promise: waitEventPromise, resolver: waitEventPromiseResovler }: CreatePromiseResult = createPromise()
-
-        window.addEventListener('popstate', async event => {
-            //User triggered event
-            const newPos = this.originalHistory.state && this.originalHistory.state.pos
-            if (!this.ignoreEvent && newPos != 1) {
-                const customEventData: NavigationEvent = { location: new URL(this.url.toString()), isHashChange: false, action: null }
-
-                // Push hash
-                if (!this.originalHistory.state || this.originalHistory.state.pos == undefined || this.originalHistory.state.pos == null) {
-                    //Prevent handler from doing strange stuff
-                    this.ignoreEvent++
-
-                    if (this.options.allowHashchange) {
-                        //new pushed url
-                        this.url = window.location.href
-                        //The event new location is different on navigate event and only on navigate event
-                        customEventData.location = new URL(this.url)
-                    }
-
-                    //return to pos 1 from pos undefined ==> 2
-                    this.originalHistory.back()
-                    //wait back event finish
-                    await waitEventTrigger()
-
-                    //Actually the only way this event is trigerred by popstate is by a hashchange
-                    customEventData.isHashChange = true
-
-                    if (this.options.listenHashchange) {
-                        //Launch event
-                        this.launchEvent(Action.hashchange, customEventData)
-                    }
-
-                    // Disable forward if neccesary
-                    if (!this.forwardButtonEnabled) this.disableForwardButton()
-
-                    //end prevent handler from doing strange stuff
-                    this.ignoreEvent--
-                }
-                //Forward event
-                else if (this.originalHistory.state.pos == 2) {
-                    this.ignoreEvent++
-                    this.originalHistory.back()
-                    //Wait back event finish
-                    await waitEventTrigger()
-                    //Launch event
-                    this.launchEvent(Action.forward, customEventData)
-                    this.ignoreEvent--
-                }
-                //Backward event
-                else if (this.originalHistory.state.pos == 0) {
-                    //Use last known location, don't change url
-                    const href = this._url.href
-                    //this.originalHistory.pushState ( back state )
-                    this.originalHistory.pushState({ pos: 1 }, '', href)
-                    // Reenable forward button
-                    if (this.forwardButtonEnabled) this.enableForwardButton()
-                    //Launch event
-                    this.launchEvent(Action.back, customEventData)
-                }
-                //set correct url
-                this.originalHistory.replaceState(this.originalHistory.state, '', this.url.toString())
-                //Reset value
-                this._nextUrl = null
-                //Save new location
-                if (this._disableForward) {
-                    await this._disableForwardButton()
-                    this._disableForward = false
-                } else if (this._enableForward) {
-                    await this._enableForwardButton()
-                    this._enableForward = false
-                }
+            if (this._forwardButtonEnabledNext) {
+                // Allow keep forward button enable
+                this.nativeHistory.forward()
+            } else {
+                // Disable forward button
+                this.nativeHistory.pushState({ pos: 1 }, '', this._url)
+                this._lastPos = 1
+                this._forwardButtonEnabled = false
             }
 
-            waitEventPromiseResovler(event)
-            const { promise, resolver } = createPromise()
-            waitEventPromise = promise
-            waitEventPromiseResovler = resolver
-        })
+            // Optional, emit event
+            if (this._preventBackEvent === 0) {
+                this.eventQueue.push({ action: Action.back, url: new URL(this._url) })
+            } else {
+                this._preventBackEvent--
+            }
 
-        function waitEventTrigger() {
-            const promise = waitEventPromise
-            const resolver = waitEventPromiseResovler
+            if (!this._forwardButtonEnabledNext) this.emitEvents()
 
-            //Resolve promise in 100 ms if for some reaason doesn't get resolved by event (ITS NECESARY ON IE)
-            let resolved = false
-            promise.then(() => { resolved = true })
-            setTimeout(function () {
-                if (!resolved) resolver()
-            }, 100)
-            //////
+        } else if (pos === 2) {
+            // Forward clicked
+            this._lastPos = 2
+            this.nativeHistory.back()
 
-            return promise
+            this.eventQueue.push({ action: Action.forward, url: new URL(this._url) })
+
+        } else if (pos === 1) {
+            // Set correct url
+            this.nativeHistory.replaceState(state, '', this._url)
+
+            // Previous position was 1 (hashchange or forward)
+            if (this._lastPos === 2 && (!this._forwardButtonEnabledNext && this._forwardButtonEnabled)) {
+                // this makes that back event will be only used to disable forward
+                this._preventBackEvent++
+                this.nativeHistory.back()
+
+            } else if (this._lastPos === 0 && (this._forwardButtonEnabledNext && !this._forwardButtonEnabled)) {
+                // Enable forward button
+                this.nativeHistory.pushState({ pos: 2 }, '', this._url)
+                this._lastPos = 1
+                this.nativeHistory.back()
+                this._forwardButtonEnabled = true
+
+            } else if (this._lastPos === 0) {
+                this._lastPos = 1
+            } else if (this._lastPos === 2) {
+                this._lastPos = 1
+            }
+            if (this._lastPos === 1) {
+                this.emitEvents()
+            }
         }
-
-        return waitEventTrigger
     }
 
+    emitEvents() {
+        // Emit events
+        for (const event of this.eventQueue) {
+            const listenersList = Array.from(this.listeners.values())
+            if (this.options.callListenersAsStack) listenersList.reverse()
+            listenersList.forEach(l => l(event))
+        }
+        this.eventQueue = []
+    }
+
+    enableForwardButton() {
+        if (this._forwardButtonEnabledNext) return
+        this._forwardButtonEnabledNext = true
+        if (this._lastPos == 1 && !this._forwardButtonEnabled) {
+            this._preventBackEvent++
+            this.nativeHistory.back()
+        }
+    }
+
+    disableForwardButton() {
+        if (!this._forwardButtonEnabledNext) return
+        this._forwardButtonEnabledNext = false
+        if (this._lastPos == 1 && this._forwardButtonEnabled) {
+            this._preventBackEvent++
+            this.nativeHistory.back()
+        }
+    }
+
+    set url(url: string | URL) {
+        this._url = (new URL(url.toString(), this._url)).href
+        if (this._lastPos === 1) {
+            this.nativeHistory.replaceState(this.nativeHistory.state, '', this._url)
+        }
+    }
+
+    listen(listener: Function) {
+        this.listeners.add(listener)
+        return () => this.listeners.delete(listener)
+    }
 }
 
-export default NavKeysController
+function initHistory(history: History, url: string) {
+    history.replaceState({ pos: 0 }, '', url)
+    history.pushState({ pos: 1 }, '', url)
+}
